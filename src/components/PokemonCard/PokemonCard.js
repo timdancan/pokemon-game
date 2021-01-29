076ccaeedcf566c8a11c569f7ba1a7e5504fd9ed
timdancan <saqe789@gmail.com>
2021-01-29 15:05:47 +0200

    homework 3 (доработка функци handleClick)

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -5,7 +5,7 @@ import cardBackSide from '../../assets/card-back-side.jpg'
 const PokemonCard = ({name, img, id, type, values})=> {
   const [isActive, setActive] = useState(false)
   const handleClick = ()=> {
-    setActive(true)
+    isActive ? setActive(false) : setActive(true)
   }
   return (
     <div className={`${s.root} ${isActive ? s.active : ''}`} onClick={handleClick}>
